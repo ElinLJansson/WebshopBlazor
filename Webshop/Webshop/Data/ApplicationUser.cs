db4f125da39a81cc20483867202f6244f8c0e7f4
Elin Jansson <99elinjansson@gmail.com>
2024-05-07 16:52:34 +0200

    UPD

--- Webshop/Webshop/Data/ApplicationUser.cs
+++ Webshop/Webshop/Data/ApplicationUser.cs
@@ -4,7 +4,6 @@ using Webshop.Models;
 
 namespace Webshop.Data;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
     public ShoppingCart ShoppingCart { get; set; }
